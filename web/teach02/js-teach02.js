36791f082494480a4d350b41053dcefea94e79e5
Brandon Francois <bafrancois@gmail.com>
2018-09-26 20:55:03 -0700

    teach 02 stretch requirements

--- web/teach02/js-teach02.js
+++ web/teach02/js-teach02.js
@@ -7,3 +7,16 @@ function changeColor() {
     var newColor = document.getElementById("color-input").value;
     div1.style.backgroundColor = newColor;
 }
+
+$(document).ready(function(){
+    $("#color-button").click(function(){
+        var newColor = $("#color-input").val();
+        $("#div1").css("background-color", newColor);
+    });
+});
+
+$(document).ready(function(){
+    $("#fade-button").click(function(){
+        $("#div3").fadeToggle();
+    });
+});
