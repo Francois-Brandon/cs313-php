e9e61bddb446af98a8e8514f58c2f4150a428bd9
Brandon Francois <bafrancois@gmail.com>
2018-10-01 14:31:44 -0700

    teach03 requirements 1 and 2

--- web/teach03/form_submit.php
+++ web/teach03/form_submit.php
@@ -1,10 +1,7 @@
 <html>
 <head>
     <title>Teach 02: Team Activity</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="mystyle.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script type="text/javascript" src="js-teach02.js"></script>
     
 </head>
 <body>
