8e0772dde061363f8e2333295065fae6caddb66c
Brandon Francois <bafrancois@gmail.com>
2018-10-01 15:07:51 -0700

    teach03 requirement 3

--- web/teach03/form_submit.php
+++ web/teach03/form_submit.php
@@ -11,6 +11,22 @@
     $sendEmail = "mailto:" . $_POST["email"];
     echo "Email: <a href=\"" . $sendEmail . "\">" . $email . "</a>" ?><br>
     Major: <?php echo $_POST["major"]; ?><br>
+    
+    <?php
+        $visited = $_POST['continents'];
+        if(empty($visited)) {
+            echo("No continents selected. you must be from space.");
+        }
+        else {
+            $N = count($visited);
+            echo("Continents Visited: ");
+            for($i=0; $i < $N; $i++) {
+                echo($visited[$i] . " ");
+            }
+        }
+    ?>
+
+    
     Comments: <?php echo $_POST["comments"]; ?><br>
     
 </body>
