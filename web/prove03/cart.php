a0c69068234f571328d711a1e7217b68fff50784
Brandon Francois <bafrancois@gmail.com>
2018-10-04 12:57:14 -0700

    prove03 shopping cart updates

--- web/prove03/cart.php
+++ web/prove03/cart.php
@@ -17,6 +17,7 @@ include 'cart-actions.php';
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="mystyle.css">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
@@ -49,18 +50,18 @@ include 'cart-actions.php';
                                 $price = $item['price'];
                                 $img = $item['img'];
                                 $quantity = $item['quantity'];
-                                
+                                $subtotal = $price * $quantity;
                                 echo "<tr>
                                     <td data-th=\"Product\">
                                         <div class=\"row\">
                                             <div class=\"col-sm-2 hidden-xs\"><img src=\"$img\" alt=\"...\" class=\"img-responsive\"/></div>
                                             <div class=\"col-sm-10\">
-                                                <h4 class=\"nomargin\">Product 1</h4>
-                                                <p>Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet.</p>
+                                                <h4 class=\"nomargin\">$name</h4>
+                                                <p>Description</p>
                                             </div>
                                         </div>
                                     </td>
-                                    <td data-th=\"Price\">$1.99</td>
+                                    <td data-th=\"Price\">$price</td>
                                     <td data-th=\"Quantity\">
                                         <input type=\"number\" class=\"form-control text-center\" value=\"1\">
                                     </td>
