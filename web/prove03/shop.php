deeab3c3fdb21fdc8e2d8626308c0b7b14747423
Brandon Francois <bafrancois@gmail.com>
2018-10-04 15:05:31 -0700

    prove03 shopping cart updates

--- web/prove03/shop.php
+++ web/prove03/shop.php
@@ -4,12 +4,12 @@
 
     if (empty($_SESSION['cart'])) {
         $_SESSION['cart'] = array(
-        'archon' => array('price' => 10.99, 'quantity' => 0, 'img' => 'images/Star-Archon.jpg', 'name' => 'Archon'),
-        'boss' => array('price' => 12.99, 'quantity' => 0, 'img' => 'images/Star-Boss.jpg', 'name' => 'Boss'),
-        'colossus' => array('price' => 14.99, 'quantity' => 0, 'img' => 'images/Star-Colossus.jpg', 'name' => 'Colossus'),
-        'daedalus' => array('price' => 12.99, 'quantity' => 0, 'img' => 'images/Star-Daedalus.jpg', 'name' => 'Daedalus'),
-        'orc' => array('price' => 10.99, 'quantity' => 0, 'img' => 'images/Star-Orc.jpg', 'name' => 'Orc'),
-        'valkyrie' => array('price' => 12.99, 'quantity' => 0, 'img' => 'images/Star-Valkyrie.jpg', 'name' => 'Valkyrie')
+        'archon' => array('price' => 10.99, 'quantity' => 0, 'img' => 'images/Star-Archon.jpg', 'name' => 'Archon', 'description' => 'The Archon is a new Speed 11 Distance Driver that blends the graceful long turn of the Katana with the smooth fade of a Wraith. It has been described as a longer Valkyrie. This is the disc for players looking to master their game and rule the course.'),
+        'boss' => array('price' => 12.99, 'quantity' => 0, 'img' => 'images/Star-Boss.jpg', 'name' => 'Boss', 'description' => 'This is a fast stable driver that can handle full power throws and moderate headwinds. Advanced players and sidearm throwers will appreciate the dependable stability. The Boss has a slight high speed turn to help maximize distance with a predictable fade.'),
+        'colossus' => array('price' => 14.99, 'quantity' => 0, 'img' => 'images/Star-Colossus.jpg', 'name' => 'Colossus', 'description' => 'The Colossus is a max distance driver with predictable high speed turn and reliable fade. Although it has a wide rim, it is also relatively easy to hold and throw both backhand and forehand. If you\'ve wanted to throw a really fast driver, but couldn\'t quite handle the wide rim, try the Colossus.'),
+        'daedalus' => array('price' => 12.99, 'quantity' => 0, 'img' => 'images/Star-Daedalus.jpg', 'name' => 'Daedalus', 'description' => ''),
+        'orc' => array('price' => 10.99, 'quantity' => 0, 'img' => 'images/Star-Orc.jpg', 'name' => 'Orc', 'description' => 'The Orc is one of our most popular, straight flying distance drivers. It combines speed with accuracy for very long range, predictable flights. Great for straight ahead power shots and long hyzer shots. Suitable for powerful throwers, but still manageable by beginners in lighter weights.'),
+        'valkyrie' => array('price' => 12.99, 'quantity' => 0, 'img' => 'images/Star-Valkyrie.jpg', 'name' => 'Valkyrie', 'description' => 'In lighter weights gives new players extra distance. Lighter weights also give players extreme range when thrown downwind, while maximum weights can give excellent upwind distance. The Valkyrie’s high speed turn and flight characteristics make it great choice for long range turnover shots and rollers.')
         );
     }
 
@@ -48,21 +48,21 @@
       <div class="panel panel-primary">
         <div class="panel-heading">Archon</div>
         <div class="panel-body"><img src="images/Star-Archon.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
-        <div class="panel-footer"><a href='shop.php?id=archon&action=add'>Add to cart</a></div>
+        <div class="panel-footer">$10.99 <a href='shop.php?id=archon&action=add'>Add to cart</a></div>
       </div>
     </div>
     <div class="col-sm-4"> 
       <div class="panel panel-primary">
         <div class="panel-heading">Boss</div>
         <div class="panel-body"><img src="images/Star-Boss.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
-        <div class="panel-footer"><a href='shop.php?id=boss&action=add'>Add to cart</a></div>
+        <div class="panel-footer">$12.99 <a href='shop.php?id=boss&action=add'>Add to cart</a></div>
       </div>
     </div>
     <div class="col-sm-4"> 
       <div class="panel panel-primary">
         <div class="panel-heading">Colossus</div>
         <div class="panel-body"><img src="images/Star-Colossus.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
-        <div class="panel-footer"><a href='shop.php?id=colossus&action=add'>Add to cart</a></div>
+        <div class="panel-footer">$14.99 <a href='shop.php?id=colossus&action=add'>Add to cart</a></div>
       </div>
     </div>
   </div>
@@ -74,21 +74,21 @@
       <div class="panel panel-primary">
         <div class="panel-heading">Daedalus</div>
         <div class="panel-body"><img src="images/Star-Daedalus.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
-        <div class="panel-footer"><a href='shop.php?id=daedalus&action=add'>Add to cart</a></div>
+        <div class="panel-footer">$12.99 <a href='shop.php?id=daedalus&action=add'>Add to cart</a></div>
       </div>
     </div>
     <div class="col-sm-4"> 
       <div class="panel panel-primary">
         <div class="panel-heading">Orc</div>
         <div class="panel-body"><img src="images/Star-Orc.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
-        <div class="panel-footer"><a href='shop.php?id=orc&action=add'>Add to cart</a></div>
+        <div class="panel-footer">$10.99 <a href='shop.php?id=orc&action=add'>Add to cart</a></div>
       </div>
     </div>
     <div class="col-sm-4"> 
       <div class="panel panel-primary">
         <div class="panel-heading">Valkyrie</div>
         <div class="panel-body"><img src="images/Star-Valkyrie.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
-        <div class="panel-footer"><a href='shop.php?id=valkyrie&action=add'>Add to cart</a></div>
+        <div class="panel-footer">$12.99 <a href='shop.php?id=valkyrie&action=add'>Add to cart</a></div>
       </div>
     </div>
   </div>
