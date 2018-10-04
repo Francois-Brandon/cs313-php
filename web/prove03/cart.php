0ad933114b2c0beb0f6fb17b5a8e3e509f7168e8
Brandon Francois <bafrancois@gmail.com>
2018-10-04 13:13:08 -0700

    prove03 shopping cart updates

--- web/prove03/cart.php
+++ web/prove03/cart.php
@@ -85,14 +85,19 @@ include 'cart-actions.php';
                                     foreach ($_SESSION['cart'] as $item) {
                                         $total += $item['price'] * $item['quantity'];
                                     }
-                                    echo $total;
+                                    echo "Total $total";
                                 ?>
                                 </strong></td>
 						</tr>
 						<tr>
 							<td><a href="#" class="btn btn-warning"><i class="fa fa-angle-left"></i> Continue Shopping</a></td>
 							<td colspan="2" class="hidden-xs"></td>
-							<td class="hidden-xs text-center"><strong>Total $1.99</strong></td>
+							<td class="hidden-xs text-center"><strong><?php
+                                    foreach ($_SESSION['cart'] as $item) {
+                                        $total += $item['price'] * $item['quantity'];
+                                    }
+                                    echo "Total $total";
+                                ?></strong></td>
 							<td><a href="#" class="btn btn-success btn-block">Checkout <i class="fa fa-angle-right"></i></a></td>
 						</tr>
 					</tfoot>
