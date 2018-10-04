9e34d821f3b3dfd587aafb8df34b22fa54f4a447
Brandon Francois <bafrancois@gmail.com>
2018-10-04 10:26:03 -0700

    prove03 shopping cart updates

---
+++ web/prove03/header.php
@@ -0,0 +1,28 @@
+<div class="jumbotron">
+  <div class="container text-center">
+      <h1 class="logo-name">Disc Golf<br>Super Store</h1>    
+  </div>
+</div>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="items.html"><p><img src="images/dgicon.png" alt="dgicon"  width="30px" class="logo-img"> Products</p></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <!--<li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Products</a></li>
+        <li><a href="#">Contact</a></li>-->
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+          <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart cart-icon"></span><span class="cart"> Cart (<?php echo $cartCount ?>)</span></a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
--- web/prove03/shop.php
+++ web/prove03/shop.php
@@ -6,25 +6,10 @@
         $_SESSION['cart'] = array();
     }
 
-$id = $_GET['id'];
-$action = $_GET['action']; 
+    $id = $_GET['id'];
+    $action = $_GET['action']; 
 
-switch($action) { 
-
-    case "add":
-        $_SESSION['cart'][$id]++; 
-    break;
-
-    case "remove":
-        $_SESSION['cart'][$id]--;
-        if($_SESSION['cart'][$id] == 0) unset($_SESSION['cart'][$id]);
-    break;
-
-    case "empty":
-        unset($_SESSION['cart']); 
-    break;
-
-}
+    include 'cart-actions.php';
 
     $cartCount = 0;
     foreach($_SESSION['cart'] as $item) {
@@ -47,54 +32,27 @@ switch($action) {
 </head>
 <body>
 
-<div class="jumbotron">
-  <div class="container text-center">
-      <h1 class="logo-name">Disc Golf<br>Super Store</h1>    
-  </div>
-</div>
-
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="items.html"><p><img src="images/dgicon.png" alt="dgicon"  width="30px" class="logo-img"> Products</p></a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <!--<li class="active"><a href="#">Home</a></li>
-        <li><a href="#">Products</a></li>
-        <li><a href="#">Contact</a></li>-->
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-          <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart cart-icon"></span><span class="cart"> Cart (<?php echo $cartCount ?>)</span></a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+<?php include 'header.php'; ?>
 
 <div class="container">    
   <div class="row">
     <div class="col-sm-4">
       <div class="panel panel-primary">
-        <div class="panel-heading">Disc #1</div>
+        <div class="panel-heading">Archon</div>
         <div class="panel-body"><img src="images/Star-Archon.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
         <div class="panel-footer"><a href='shop.php?id=archon&action=add'>Add to cart</a></div>
       </div>
     </div>
     <div class="col-sm-4"> 
       <div class="panel panel-primary">
-        <div class="panel-heading">Disc #2</div>
+        <div class="panel-heading">Boss</div>
         <div class="panel-body"><img src="images/Star-Boss.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
         <div class="panel-footer"><a href='shop.php?id=boss&action=add'>Add to cart</a></div>
       </div>
     </div>
     <div class="col-sm-4"> 
       <div class="panel panel-primary">
-        <div class="panel-heading">Disc #3</div>
+        <div class="panel-heading">Colossus</div>
         <div class="panel-body"><img src="images/Star-Colossus.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
         <div class="panel-footer"><a href='shop.php?id=colossus&action=add'>Add to cart</a></div>
       </div>
@@ -106,21 +64,21 @@ switch($action) {
   <div class="row">
     <div class="col-sm-4">
       <div class="panel panel-primary">
-        <div class="panel-heading">Disc #4</div>
+        <div class="panel-heading">Daedalus</div>
         <div class="panel-body"><img src="images/Star-Daedalus.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
         <div class="panel-footer"><a href='shop.php?id=daedalus&action=add'>Add to cart</a></div>
       </div>
     </div>
     <div class="col-sm-4"> 
       <div class="panel panel-primary">
-        <div class="panel-heading">Disc #5</div>
+        <div class="panel-heading">Orc</div>
         <div class="panel-body"><img src="images/Star-Orc.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
         <div class="panel-footer"><a href='shop.php?id=orc&action=add'>Add to cart</a></div>
       </div>
     </div>
     <div class="col-sm-4"> 
       <div class="panel panel-primary">
-        <div class="panel-heading">Disc #6</div>
+        <div class="panel-heading">Valkyrie</div>
         <div class="panel-body"><img src="images/Star-Valkyrie.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
         <div class="panel-footer"><a href='shop.php?id=valkyrie&action=add'>Add to cart</a></div>
       </div>
