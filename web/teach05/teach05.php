6c07e6a6b3243d614e04ff84b75afd38ebe93880
Brandon Francois <bafrancois@gmail.com>
2018-10-15 15:10:44 -0700

    teach05 initial commit

---
+++ web/teach05/teach05.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>Teach 02: Team Activity</title>
+    <link rel="stylesheet" type="text/css" href="mystyle.css">
+</head>
+<body>
+    <h1>Scripture Resources</h1>
+    
+</body>
+</html>
