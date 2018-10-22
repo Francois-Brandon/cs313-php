cb7abddc589008f30d266316915d9beafef26ebd
Brandon Francois <bafrancois@gmail.com>
2018-10-22 15:11:47 -0700

    recipebox working on submit recipe

--- web/recipebox/submitrecipe.php
+++ web/recipebox/submitrecipe.php
@@ -10,10 +10,26 @@
     <link rel="stylesheet" type="text/css" href="recipe-style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="res/js-recipebox.js" type="text/javascript"></script>
 </head>
 <body>
     
 <?php require 'res/nav.php'; ?>
+    
+<div class="container results-container">
+	<div class="row">
+		<input type="hidden" name="count" value="1" />
+        <div class="control-group" id="fields">
+            <label class="control-label" for="field1">Ingredients</label>
+            <div class="controls" id="profs"> 
+                <form class="input-append">
+                    <div id="field"><input autocomplete="off" class="input" id="field1" name="prof1" type="text" placeholder="1 Cup Sugar" data-items="8"/><button id="b1" class="btn add-more" type="button">+</button></div>
+                </form>
+            <br>
+            </div>
+        </div>
+	</div>
+</div>
 
     
    <?php
