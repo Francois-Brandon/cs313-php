ce290ace52c82215b0c6ce3ecf34a6263c9c437e
Brandon Francois <bafrancois@gmail.com>
2018-10-22 14:00:14 -0700

    recipebox working on search

--- web/recipebox/search.php
+++ web/recipebox/search.php
@@ -1,4 +1,10 @@
-<?php require 'res/db.php'; ?>
+<?php
+    
+    session_start();
+
+    require 'res/db.php';
+
+?>
 
 <html lang="en">
 <head>
@@ -15,24 +21,71 @@
     
 <?php require 'res/nav.php'; ?>
 
-<div class="container search-container">
-    <div class="row search-row">
-        <div class="col-sm-12">
-            <form action="search.php" method="post">
-                <button type="submit"><i class="fa fa-search"></i></button>
-                <input type="text" name="recipe-search" class="recipe-search-bar" placeholder="Search for a recipe by keyword">
-            </form>
-        </div>
-    </div>
-</div>
+<div class="container recipe-container"> 
+<h1>Results</h1>
+  <div class="row">
     
    <?php
-        //foreach ($db->query('SELECT * FROM scriptures') as $row)
-        //{
-        //  echo '<p><strong>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</strong> //- "' . $row['content'] . '"';
-        //  echo '</p>';
-        //}
+    $search = $_POST['recipe-search'];
+    
+    $stmt = $db->prepare('SELECT c.name AS name, r.recipe_id AS recipe_id, r.recipe_name AS recipe_name, r.directions AS directions, r.date_created FROM recipe AS r JOIN contributor AS c ON r.contributor_id = c.contributor_id WHERE r.recipe_name LIKE %:search% ORDER BY r.date_created');
+    $stmt->bindValue(':search', $search, PDO::PARAM_INT);
+    $stmt->execute();
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($rows as $row)
+    {
+        
+        $recipe_name = htmlspecialchars($row['recipe_name']);
+        $recipe_id = htmlspecialchars($row['recipe_id']);
+        $ingredients = '';
+        $directions = htmlspecialchars($row['directions']);
+        
+        $stmt = $db->prepare('SELECT item FROM ingredients WHERE recipe_id=:recipe_id');
+        $stmt->bindValue(':recipe_id', $recipe_id, PDO::PARAM_INT);
+        $stmt->execute();
+        $ingredientrows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+        foreach ($ingredientrows as $value) {
+            $ingredients .= htmlspecialchars($value['item']) . '<br>';
+        }
+        
+        echo '<div class="col-sm-4">';
+            echo '<div class="panel panel-primary">';
+                echo '<div class="panel-heading">' . $recipe_name . '</div>';
+                echo '<div class="panel-body">' . $ingredients . '</div>';
+                echo '<div class="panel-footer"><a data-toggle="modal" href=\'\#' . $recipe_id . '-modal\'>See More</a></div>';
+            echo '</div>';
+        echo '</div>';
+
+            
+        echo "<div id=\"" . $recipe_id . "-modal\" class=\"modal fade\" role=\"dialog\">
+            <div class=\"modal-dialog\">
+
+                <div class=\"modal-content\">
+                    <div class=\"modal-header\">
+                        <button type=\"button\" class=\"close\" data-dismiss=\"modal\">&times;</button>
+                        <h4 class=\"modal-title\">" . $recipe_name . "</h4>
+                    </div>
+                    <div class=\"modal-body\">
+
+                        <p>" . $ingredients . "</p>
+                        <p>" . $directions . "</p>
+                    </div>
+                    <div class=\"modal-footer\">
+                        <button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>";
+        
+    }
+    
     ?>
+      
+    </div>
+</div>
     
 <?php require 'res/footer.php'; ?>
     
