23b0753ab279b6ecfcf4932640bfdbfbe4950498
Brandon Francois <bafrancois@gmail.com>
2018-10-23 12:03:41 -0700

    recipebox working on submit recipe

--- web/recipebox/submitrecipe.php
+++ web/recipebox/submitrecipe.php
@@ -37,60 +37,12 @@
 	</div>
 </div>-->
     
-<div class="container results-container">
-   <form id="regForm" action="">
 
-        <h1>Register:</h1>
-
-        <!-- One "tab" for each step in the form: -->
-        <div class="tab">Name:
-          <p><input placeholder="First name..." oninput="this.className = ''"></p>
-          <p><input placeholder="Last name..." oninput="this.className = ''"></p>
-        </div>
-
-        <div class="tab">Contact Info:
-          <p><input placeholder="E-mail..." oninput="this.className = ''"></p>
-          <p><input placeholder="Phone..." oninput="this.className = ''"></p>
-        </div>
-
-        <div class="tab">Birthday:
-          <p><input placeholder="dd" oninput="this.className = ''"></p>
-          <p><input placeholder="mm" oninput="this.className = ''"></p>
-          <p><input placeholder="yyyy" oninput="this.className = ''"></p>
-        </div>
-
-        <div class="tab">Login Info:
-          <p><input placeholder="Username..." oninput="this.className = ''"></p>
-          <p><input placeholder="Password..." oninput="this.className = ''"></p>
-        </div>
-
-        <div style="overflow:auto;">
-          <div style="float:right;">
-            <button type="button" id="prevBtn" onclick="nextPrev(-1)">Previous</button>
-            <button type="button" id="nextBtn" onclick="nextPrev(1)">Next</button>
-          </div>
-        </div>
-
-        <!-- Circles which indicates the steps of the form: -->
-        <div style="text-align:center;margin-top:40px;">
-          <span class="step"></span>
-          <span class="step"></span>
-          <span class="step"></span>
-          <span class="step"></span>
-        </div>
-
-    </form>
-</div>
     
-<!--    <div class="row">
+<div class="row">
     <div class="col-md-6 col-md-offset-3">
         <form id="msform">
 
-            <ul id="progressbar">
-                <li class="active">Ingredients</li>
-                <li>Directions</li>
-                <li>Image</li>
-            </ul>
             
             <fieldset>
                 <h2 class="fs-title">Ingredients</h2>
@@ -103,25 +55,25 @@
                             </button>
                         </span>
                     
-                <input type="button" name="next" class="next action-button" value="Next"/>
+                
             </fieldset>
             <fieldset>
                 <h2 class="fs-title">Directions</h2>
                 <h3 class="fs-subtitle">Enter the directions for your recipe</h3>
                 <textarea name="input-directions" rows="20" cols="100" placeholder="Directions"></textarea>
-                <input type="button" name="previous" class="previous action-button-previous" value="Previous"/>
-                <input type="button" name="next" class="next action-button" value="Next"/>
+
+
             </fieldset>
             <fieldset>
                 <h2 class="fs-title">Upload Image</h2>
                 <h3 class="fs-subtitle">Select image to upload</h3>
                 <input type="file" name="fileToUpload" id="fileToUpload">
-                <input type="button" name="previous" class="previous action-button-previous" value="Previous"/>
+
                 <input type="submit" name="submit" class="submit action-button" value="Submit"/>
             </fieldset>
         </form>
     </div>
-</div>-->
+</div>
 
     
    <?php
