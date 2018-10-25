139972970c32ae231a99878bc25d63ef4e0a5f74
Brandon Francois <bafrancois@gmail.com>
2018-10-25 10:28:56 -0700

    recipebox working on submit recipe

--- web/recipebox/insertrecipe.php
+++ web/recipebox/insertrecipe.php
@@ -9,7 +9,6 @@ $directions = $_POST['input-directions'];
 $name = $_POST['recipe-name'];
 $username = $_SESSION['username'];
 
-echo 'Username: ' . $username;
 
 $query = 'SELECT id FROM login WHERE username = :username';
 $statement = $db->prepare($query);
@@ -17,8 +16,6 @@ $statement->bindValue(':username', $username);
 $statement->execute();
 $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-echo '<br>Query result1: ' . $rows['id'];
-echo '<br>Query result2: ' . $rows[0]['id'];
 
 foreach ($rows as $row) {
     $user_id = $row['id'];
@@ -26,7 +23,7 @@ foreach ($rows as $row) {
 
 echo '<br>User ID: ' . $user_id;
 
-/*$query = 'INSERT INTO recipe (recipe_name, user_id, directions) VALUES(:name, :user_id, :directions)';
+$query = 'INSERT INTO recipe (recipe_name, user_id, directions) VALUES(:name, :user_id, :directions)';
 $statement = $db->prepare($query);
 $statement->bindValue(':name', $name);
 $statement->bindValue(':user_id', $user_id);
@@ -47,6 +44,6 @@ $statement->execute();
 }
 
 header("Location: home.php");
-die();*/
+die();
 
 ?>
