c0649f47b5303458ce001834356a062dd8bd1d64
Brandon Francois <bafrancois@gmail.com>
2018-10-27 18:22:34 -0700

    recipebox working on edit/delete my recipe

--- web/recipebox/editrecipe.php
+++ web/recipebox/editrecipe.php
@@ -95,65 +95,7 @@
         </div>
 	</div>
 </div>
-    
 
- <!--<div class="container results-container">  
-    <div class="row">
-    <div class="col-md-6 col-md-offset-3">
-        <form id="msform">
-
-            
-            <fieldset>
-                <h2 class="fs-title">Ingredients</h2>
-                <h3 class="fs-subtitle">Enter the ingredients for your recipe</h3>
-                
-                <div class="row">
-                    <div class="control-group" id="fields">
-                        <label class="control-label" for="field1">Ingredients</label>
-                        <div class="controls"> 
-                            <form role="form" autocomplete="off">
-                                <div class="entry input-group col-xs-3">
-                                    <input class="form-control" name="fields[]" type="text" placeholder="1 Cup Sugar" />
-                                    <span class="input-group-btn">
-                                        <button class="btn btn-success btn-add" type="button">
-                                            <span class="glyphicon glyphicon-plus"></span>
-                                        </button>
-                                    </span>
-                                </div>
-                            </form>
-                        <br>
-                        </div>
-                    </div>
-                </div>
-                    
-                
-            </fieldset>
-            <fieldset>
-                <h2 class="fs-title">Directions</h2>
-                <h3 class="fs-subtitle">Enter the directions for your recipe</h3>
-                <textarea name="input-directions" rows="20" cols="100" placeholder="Directions"></textarea>
-
-
-            </fieldset>
-            <fieldset>
-                <h2 class="fs-title">Upload Image</h2>
-                <h3 class="fs-subtitle">Select image to upload</h3>
-                <input type="file" name="fileToUpload" id="fileToUpload">
-
-                <input type="submit" name="submit" class="submit action-button" value="Submit"/>
-            </fieldset>
-        </form>
-    </div>
-</div>
-</div> -->
-    
-   <?php
-        //foreach ($db->query('SELECT * FROM scriptures') as $row)
-        //{
-        //  echo '<p><strong>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</strong> //- "' . $row['content'] . '"';
-        //  echo '</p>';
-        //}
-    ?>
     
 <?php require 'res/footer.php'; ?>
     
