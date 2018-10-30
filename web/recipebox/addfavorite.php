4dfdb8d908d3008b05ba005437cc81113da5296e
Brandon Francois <bafrancois@gmail.com>
2018-10-29 21:04:27 -0700

    recipebox working on favorites

---
+++ web/recipebox/addfavorite.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+    
+require 'res/db.php';
+
+$recipe_id = $_POST['recipe_id'];
+$username = $_SESSION['username'];
+
+
+$query = 'SELECT id FROM login WHERE username = :username';
+$statement = $db->prepare($query);
+$statement->bindValue(':username', $username);
+$statement->execute();
+$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+foreach ($rows as $row) {
+    $user_id = $row['id'];
+}
+    
+$query = 'INSERT INTO favorites (recipe_id, user_id) VALUES(:recipe_id, :user_id)';
+$statement = $db->prepare($query);
+$statement->bindValue(':recipe_id', $recipe_id);
+$statement->bindValue(':user_id', $user_id);
+$statement->execute();
+    
+
+
+?>
