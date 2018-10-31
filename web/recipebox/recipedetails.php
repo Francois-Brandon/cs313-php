626afe3751ae2ab0854638e127b7ba6b8ad15988
Brandon Francois <bafrancois@gmail.com>
2018-10-31 12:50:15 -0700

    recipebox working on ratings

--- web/recipebox/recipedetails.php
+++ web/recipebox/recipedetails.php
@@ -67,7 +67,7 @@
                         <td>
                           <input id="star-input" name="star-input" value="' . $avg . '" class="rating-loading">
                         </td>
-                        <td><label class="control-label">' . $count . ' Reviews</label></td>
+                        <td><label class="control-label">' . $count . '   Reviews</label></td>
                       </tr>
                     </table>
                 <script>
