4b396c691fac8d79bcf0374e9987a110c5fefaa3
Brandon Francois <bafrancois@gmail.com>
2018-10-31 20:49:28 -0700

    recipebox working on ratings

--- web/recipebox/recipedetails.php
+++ web/recipebox/recipedetails.php
@@ -210,7 +210,7 @@
             </form>';
         } 
         else {
-            echo '<h3 class="center center-padding"><a href="signin.php">Sign In to review this recipe!</a></h3>';
+            echo '<h3 class="center-padding center"><a href="signin.php">Sign In to review this recipe!</a></h3>';
         }
         
         ?>
