32f8de5428ba7d8b871c40d0d7575e9d549fd583
Brandon Francois <bafrancois@gmail.com>
2018-11-01 08:04:44 -0700

    recipebox working on ratings

--- web/recipebox/home.php
+++ web/recipebox/home.php
@@ -13,10 +13,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="res/star-rating.min.css">
     <link rel="stylesheet" type="text/css" href="recipe-style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inview/1.0.0/jquery.inview.min.js"></script>
+    <script src="res/star-rating.min.js" type="text/javascript"></script>
 </head>
 <body>
     
